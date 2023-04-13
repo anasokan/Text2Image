d66ce9787a0c0c73d47df46c71af51417621e934
subburajav <61749092+subburajav@users.noreply.github.com>
2023-04-13 10:23:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def generate_image():
     return render_template("image.html", image_url=image_url)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
